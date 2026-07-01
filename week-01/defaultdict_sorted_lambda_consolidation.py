0d3ef6ff3971ad4658227b10d41c3077ee4279de
JFBrunnerDev <jf.brunner.dev@gmail.com>
2026-07-01 17:51:54 +0200

    add defaultdict_sorted_lambda_consolidation

---
+++ week-01/defaultdict_sorted_lambda_consolidation.py
@@ -0,0 +1,27 @@
+transactions = [
+    ("Alice", 120, "food"),
+    ("Bob", 45, "tech"),
+    ("Alice", 30, "tech"),
+    ("Charlie", 200, "food"),
+    ("Bob", 15, "food"),
+    ("Alice", 90, "food"),
+    ("Charlie", 60, "tech"),
+]
+
+from collections import defaultdict
+
+def filtering(method):
+    d=defaultdict(list)
+    for x in method:
+        d[x[2]].append((x[0], x[1]))
+
+    result={}
+    for category, lists in d.items():
+        f=defaultdict(int)
+        for name, cost in lists:
+            f[name] += cost
+        result[category] = sorted(f.items(), key=lambda x: x[1], reverse=True)
+        
+    return result
+
+print(filtering(transactions))
